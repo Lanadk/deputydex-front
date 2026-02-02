0c5dbcb0578dc30edcefa3487d5b4ab1108cb867
Lanadk <ottmanbecuwe@gmail.com>
2026-02-02 16:34:59 +0100

    poc: github actions #11

---
+++ scripts/github_actions_tests/test-poc.ts
@@ -0,0 +1,13 @@
+async function main() {
+    console.log('🚀 POC Script TypeScript\n');
+
+    const a = 10;
+    const b = 32;
+    const result = a + b;
+
+    console.log(`Addition: ${a} + ${b} = ${result}`);
+
+    console.log('\n Script terminé avec succès');
+}
+
+main();
